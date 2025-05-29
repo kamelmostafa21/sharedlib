2332b2567aaa607fe7627f8493f2f91d9b17b9b0
Kamelmostafa21 <kamelmo284@gmail.com>
2025-05-29 21:03:36 +0300

    updating lib

--- src/org/iti/docker.groovy
+++ src/org/iti/docker.groovy
@@ -4,10 +4,21 @@ def login(USERNAME, PASSWORD){
     sh "docker login -u ${USERNAME} -p ${PASSWORD}"
 }
 
-def build(IMAGE_NAME, IMAGE_TAG){
-    sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+def buildPython(IMAGE_NAME, IMAGE_TAG){
+    dir('python'){
+        sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+    }
 }
+    
 
 def push(IMAGE_NAME, IMAGE_TAG){
     sh "docker push ${IMAGE_NAME}:${IMAGE_TAG}"
 }
+
+def buildJava(IMAGE_NAME, IMAGE_TAG){
+    dir('java'){
+        sh "mvn clean package install -Dmaven.test.skip=true"
+        sh "docker build -t ${IMAGE_NAME}:${IMAGE_TAG} ."
+    }
+    
+} 
